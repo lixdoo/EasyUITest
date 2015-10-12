274ab883e3000b9e364267a6d042cb984fe78f01
lixdoo <437526826@qq.com>
2015-10-12 22:37:56 +0800

    UserEntity

--- EasyUiPractice/src/lixdoo/practise/entity/UserEntity.java
+++ EasyUiPractice/src/lixdoo/practise/entity/UserEntity.java
@@ -20,12 +20,30 @@ public class UserEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
-	@Column(name="id",unique =true,nullable = false)
+	@Column(name="id",unique =true,nullable = false) //主键
 	private Integer id;
 	private String cardId;
 	private String name;
 	private String age;
 	private Boolean gender;
+	
+	public UserEntity(){
+		
+	}
+	//minimal constructor
+	public UserEntity(String cardId,String name,String age){
+		this.cardId = cardId;
+		this.name = name;
+		this.age= age;
+		
+	}
+	//full constructor
+	public UserEntity(String cardId,String name,String age,Boolean gender ){
+		this.cardId = cardId;
+		this.name = name;
+		this.age= age;
+		this.gender = gender;
+	}
 	public String getName() {
 		return name;
 	}
